e9d692788fa99c212790e606368ca9ff72238aba
Albert <albert8522@gmail.com>
2022-07-31 17:02:26 +0800

    07.尚硅谷_node基础_模块化详解

---
+++ 7.module.js
@@ -0,0 +1,18 @@
+// var math =require("./math.js");
+
+// console.log(math.add(123,456));
+
+// var fs = require("fs");
+// console.log(fs);
+
+// a =10;
+// console.log(global.a);
+
+// console.log(arguments);
+// console.log(arguments.callee+"");
+// console.log(arguments.length);
+// console.log(exports);
+// console.log(module.exports);
+// console.log(module.exports==exports);
+console.log(__filename);
+console.log(__dirname);
